e7530b504a65342dab8f75395777b972ba53b166
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-06-26 17:20:11 -0400

    Power diversion effects attack power

--- src/MechaController.js
+++ src/MechaController.js
@@ -4,14 +4,15 @@ class MechaController{
     constructor(){}
 
     attack(attacker, defender){
+        let attackPower = attacker.attack * attacker.powerDiverter.divertList[SYSTEM_ATTACK] / 100;
         if (defender.shields > 0){
-            defender.shields -= attacker.attack;
+            defender.shields -= attackPower;
             if (defender.shields < 0){
                 defender.shields = 0;
             }
         }
         else{
-            defender.health -= attacker.attack;
+            defender.health -= attackPower;
             if (defender.health < 0){
                 defender.health = 0;
             }
